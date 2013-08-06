080631936f20f0f41a7a625a22fd869a41516c84
Takashi AOKI <federkasten@me.com>
2013-08-06 13:26:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vocl
+
+a simple library for client/server communication via WebSocket
+
+## License
+
+Copyright © 2013 FIXME
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject vocl "0.1.0-SNAPSHOT"
+  :description "a simple library for client/server communication via WebSocket"
+  :url "http://example.com/vocl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
