7b0c8403691ed1ff0aed4652168108e84665131e
Takashi AOKI <federkasten@me.com>
2013-11-14 15:39:24 +0900

    fix project.clj issue

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject vocl "0.1.0-SNAPSHOT"
-  :description "a simple library for client/server communication via WebSocket"
-  :url "http://example.com/vocl"
+  :description "a library for client/server communication via WebSocket"
+  :url "https://chrov.is"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [aleph "0.3.0"]]
+  :plugins [[lein-midje "3.1.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
