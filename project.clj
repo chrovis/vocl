32d77bc9e9cda2272f58e4389ef8735c839c243f
Takashi AOKI <federkasten@me.com>
2013-11-15 16:36:26 +0900

    add repositories

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://chrov.is"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["snapshots" {:url "https://nexus.xcoo.jp/content/repositories/snapshots"}]
+                 ["releases" {:url "https://nexus.xcoo.jp/content/repositories/releases"}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [aleph "0.3.0"]]
   :plugins [[lein-midje "3.1.1"]]
