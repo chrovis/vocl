dcf742a6bf89580caf6e0d7334fa21473ea9b642
Takashi AOKI <federkasten@me.com>
2014-01-23 18:37:05 +0900

    remove private repository configuration from project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "https://github.com/chrovis/vocl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["snapshots" {:url "https://nexus.xcoo.jp/content/repositories/snapshots"}]
-                 ["releases" {:url "https://nexus.xcoo.jp/content/repositories/releases"}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [aleph "0.3.1"]]
   :plugins [[lein-midje "3.1.1"]]
